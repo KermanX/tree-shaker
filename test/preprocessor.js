f8411d7a80822c4baea6e3fc475e5aed18c025a1
_Kerman <kermanx@qq.com>
2024-09-15 17:11:30 +0800

    chore: update test262 preprocessor

--- test/preprocessor.js
+++ test/preprocessor.js
@@ -31,19 +31,37 @@ function printDiff(diff) {
 }
 
 let index = 0;
+let skipped = 0;
 module.exports = function(test) {
   try {
-    console.log('\n>', ++index, '----------------');
-    console.log(test.file);
     let prelude = test.contents.slice(0, test.insertionIndex);
     let main = test.contents.slice(test.insertionIndex);
+
+    if (main.includes('eval(')) {
+      console.log('\n> Skipping eval', ++skipped, '----------------');
+      console.log(test.file);
+      return test;
+    }
+    if (main.includes('$DONOTEVALUATE')) {
+      console.log('\n> Skipping $DONOTEVALUATE', ++skipped, '----------------');
+      console.log(test.file);
+      return test;
+    }
+    if (/with\s*\(/.test(main)) {
+      console.log('\n> Skipping with', ++skipped, '----------------');
+      console.log(test.file);
+      return test;
+    }
+
+    console.log('\n> Testing', ++index, '----------------');
+    console.log(test.file);
     let minified = treeShake(treeShakeEval(main, false), false, do_minify, false);
     let startTime = Date.now();
     let treeShaked = treeShake(treeShakeEval(main, true), true, do_minify, false);
     let endTime = Date.now();
     console.log(`${pc.gray(main.length)} -> ${pc.red(minified.length)} -> ${pc.green(treeShaked.length)} (${pc.yellow((treeShaked.length * 100 / minified.length).toFixed(2) + '%')}) +${endTime - startTime}ms`);
-    if (minified !== treeShaked)
-      printDiff(Diff.diffChars(minified, treeShaked));
+    // if (minified !== treeShaked && !test.file.includes('unicode'))
+    //   printDiff(Diff.diffChars(minified.slice(0, 500), treeShaked.slice(0, 500)));
     test.contents = prelude + treeShaked;
   } catch (error) {
     test.result = {
