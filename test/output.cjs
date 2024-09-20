e4cc9d1eb27ff5b3dd27f85db209a7e9f90f76d7
_Kerman <kermanx@qq.com>
2024-09-20 16:03:31 +0800

    chore: fix output.cjs

--- test/output.cjs
+++ test/output.cjs
@@ -14,7 +14,7 @@ process.stdin.on('end', () => {
     .replace(/^\[SKIP\].*$/gm, '')
     .replace(/^FAIL /gm, 'test\\')
     .replace(/ \(strict mode\)$/gm, '')
-    .replace(/^.*(default)\n.*\n/gm, '')
+    .replace(/^.*\(default\)\n.*\n/gm, '')
     .replace(/\n{3,}/gm, '\n\n');
   const resultPath = path.join(__dirname, 'result.txt');
   try {
