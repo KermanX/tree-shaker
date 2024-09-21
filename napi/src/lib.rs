74cd50ccb510292ea31321714d7389fb9c4becdf
_Kerman <kermanx@qq.com>
2024-09-21 20:58:42 +0800

    fix: napi build

--- napi/src/lib.rs
+++ napi/src/lib.rs
@@ -10,7 +10,7 @@ pub fn tree_shake(input: String, do_tree_shake: bool, do_minify: bool, eval_mode
   let result = tree_shake::tree_shake(tree_shake::TreeShakeOptions {
     config: tree_shake::TreeShakeConfig::default(),
     allocator: &Allocator::default(),
-    source_type: SourceType::default().with_module(true).with_always_strict(true),
+    source_type: SourceType::default(),
     source_text: input,
     tree_shake: do_tree_shake,
     minify: do_minify.then(|| MinifierOptions::default()),
