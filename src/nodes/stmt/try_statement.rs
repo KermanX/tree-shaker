f5f26b12415972d6534d09ea2cbddc1d6ef70545
_Kerman <kermanx@qq.com>
2024-09-24 20:07:08 +0800

    fix: try statement without catch clause may forward errors

--- src/nodes/stmt/try_statement.rs
+++ src/nodes/stmt/try_statement.rs
@@ -13,21 +13,28 @@ impl<'a> Analyzer<'a> {
 
     self.push_try_scope();
     self.exec_block_statement(&node.block);
-    let thrown_val = self.pop_try_scope().thrown_val();
+    let try_scope = self.pop_try_scope();
 
-    if let Some(handler) = &node.handler {
+    let uncaught = if let Some(handler) = &node.handler {
       self.exec_catch_clause(
         handler,
         // Theoretically, if `thrown_val` is `None`, it means that the `try` block
         // does not throw any value, so we should skip the `catch` block.
         // However, we can guarantee that all possible exceptions tracked.
         // For example, KeyboardInterrupt, which is not tracked, can be thrown.
-        thrown_val.unwrap_or_else(|| UnknownEntity::new_unknown()),
+        try_scope.thrown_val().unwrap_or_else(|| UnknownEntity::new_unknown()),
       );
-    }
+      None
+    } else {
+      try_scope.may_throw.then(|| try_scope.thrown_values.clone())
+    };
 
     if let Some(finalizer) = &node.finalizer {
       self.exec_block_statement(finalizer);
+    }
+
+    if let Some(uncaught) = uncaught {
+      self.forward_throw(uncaught.clone(), ());
     }
 
     self.pop_cf_scope();
