b1cb834232457e1fad1b1cd44877d77b3d41989e
_Kerman <kermanx@qq.com>
2024-09-28 11:07:18 +0800

    fix: exec_with_default

--- src/nodes/misc/with_default.rs
+++ src/nodes/misc/with_default.rs
@@ -11,16 +11,26 @@ impl<'a> Analyzer<'a> {
     value: Entity<'a>,
   ) -> (bool, Entity<'a>) {
     let is_undefined = value.test_is_undefined();
+
+    self.push_variable_scope_with_dep(value.clone());
     let binding_val = match is_undefined {
       Some(true) => self.exec_expression(default),
-      Some(false) => value,
+      Some(false) => value.clone(),
       None => {
         self.push_cf_scope_normal(None);
-        let value = UnionEntity::new(vec![self.exec_expression(default), value]);
+        let value = UnionEntity::new(vec![self.exec_expression(default), value.clone()]);
         self.pop_cf_scope();
         value
       }
     };
-    (is_undefined != Some(false), binding_val)
+    self.pop_variable_scope();
+
+    let need_init = is_undefined != Some(false);
+
+    if need_init {
+      self.consume(value);
+    }
+
+    (need_init, binding_val)
   }
 }
