0d163146e148798db44bfc3fdb46a6e126a87382
_Kerman <kermanx@qq.com>
2024-10-01 17:00:32 +0800

    fix: object expression with `__proto__`

--- src/nodes/expr/object_expression.rs
+++ src/nodes/expr/object_expression.rs
@@ -1,13 +1,14 @@
 use crate::{
   analyzer::Analyzer,
   build_effect,
-  entity::{Entity, ForwardedEntity},
+  entity::{Entity, EntityTrait, ForwardedEntity},
   transformer::Transformer,
 };
 use oxc::{
   ast::{
     ast::{
-      Expression, ObjectExpression, ObjectProperty, ObjectPropertyKind, PropertyKind, SpreadElement,
+      Expression, ObjectExpression, ObjectProperty, ObjectPropertyKind, PropertyKey, PropertyKind,
+      SpreadElement,
     },
     AstKind,
   },
@@ -18,23 +19,36 @@ impl<'a> Analyzer<'a> {
   pub fn exec_object_expression(&mut self, node: &'a ObjectExpression) -> Entity<'a> {
     let object = self.new_empty_object();
 
+    let mut has_proto = false;
+
     for property in &node.properties {
       match property {
         ObjectPropertyKind::ObjectProperty(node) => {
           let key = self.exec_property_key(&node.key);
           let value = self.exec_expression(&node.value);
-          object.init_property(
-            node.kind,
-            key,
-            ForwardedEntity::new(value, AstKind::ObjectProperty(node)),
-            true,
-          );
+          let value = ForwardedEntity::new(value, AstKind::ObjectProperty(node));
+
+          match &node.key {
+            PropertyKey::StaticIdentifier(node) if node.name == "__proto__" => {
+              has_proto = true;
+              // Ensure the __proto__ is consumed - it may be overridden by the next property like ["__proto__"]: 1
+              self.consume(value);
+            }
+            _ => {
+              object.init_property(node.kind, key, value, true);
+            }
+          };
         }
         ObjectPropertyKind::SpreadProperty(node) => {
           let argument = self.exec_expression(&node.argument);
           object.init_spread(self, AstKind::SpreadElement(node), argument);
         }
       }
+    }
+
+    if has_proto {
+      // Deoptimize the object
+      object.consume(self);
     }
 
     Entity::new(object)
