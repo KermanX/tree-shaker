26e05ff1944b7b4880a9d97b3263776f2ef0fd9d
_Kerman <kermanx@qq.com>
2024-10-24 20:40:21 +0800

    fix

--- src/utils.rs
+++ src/utils.rs
@@ -36,34 +36,35 @@ impl<'a> Analyzer<'a> {
     self.allocator.alloc(format!("__#private__{}", name))
   }
 
-  pub fn has_pure_notation(&self, span: Span) -> usize {
+  pub fn has_pure_notation(&self, _span: Span) -> usize {
     return 0;
 
-    let Some(comment) = self.semantic.comments_range(..span.start).next_back() else {
-      return 0;
-    };
-    let raw = comment.span.source_text(self.semantic.source_text());
+    // TODO: Pure annotation
+    // let Some(comment) = self.semantic.comments_range(..span.start).next_back() else {
+    //   return 0;
+    // };
+    // let raw = comment.span.source_text(self.semantic.source_text());
 
-    // If there are non-whitespace characters between the `comment` and the `span`,
-    // we treat the `comment` not belongs to the `span`.
-    let range_text =
-      Span::new(comment.span.end, span.start).source_text(self.semantic.source_text());
-    let only_whitespace = match comment.kind {
-      CommentKind::Line => range_text.trim().is_empty(),
-      CommentKind::Block => {
-        range_text
-          .strip_prefix("*/") // for multi-line comment
-          .is_some_and(|s| s.trim().is_empty())
-      }
-    };
-    if !only_whitespace {
-      return 0;
-    }
+    // // If there are non-whitespace characters between the `comment` and the `span`,
+    // // we treat the `comment` not belongs to the `span`.
+    // let range_text =
+    //   Span::new(comment.span.end, span.start).source_text(self.semantic.source_text());
+    // let only_whitespace = match comment.kind {
+    //   CommentKind::Line => range_text.trim().is_empty(),
+    //   CommentKind::Block => {
+    //     range_text
+    //       .strip_prefix("*/") // for multi-line comment
+    //       .is_some_and(|s| s.trim().is_empty())
+    //   }
+    // };
+    // if !only_whitespace {
+    //   return 0;
+    // }
 
-    if raw.contains("@__PURE__") || raw.contains("#__PURE__") {
-      1
-    } else {
-      0
-    }
+    // if raw.contains("@__PURE__") || raw.contains("#__PURE__") {
+    //   1
+    // } else {
+    //   0
+    // }
   }
 }
