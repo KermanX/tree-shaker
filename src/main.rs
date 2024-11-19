4ee1bbcd18ab36052f92900ec3d314b6ee8206e4
_Kerman <kermanx@qq.com>
2024-11-19 19:45:33 +0800

    feat(cli): report size

--- src/main.rs
+++ src/main.rs
@@ -34,31 +34,50 @@ fn main() {
 
   let start_time = std::time::Instant::now();
 
-  let result = tree_shake(
-    content,
+  let minified = tree_shake(
+    content.clone(),
     TreeShakeOptions {
-      config: if args.no_shake {
-        TreeShakeConfig::disabled()
-      } else {
-        TreeShakeConfig::recommended()
-      },
-      minify_options: args.minify.then(MinifierOptions::default),
-      codegen_options: CodegenOptions {
-        minify: args.minify,
-        comments: !args.minify,
-        ..Default::default()
-      },
+      config: TreeShakeConfig::disabled(),
+      minify_options: Some(MinifierOptions::default()),
+      codegen_options: CodegenOptions { minify: true, comments: false, ..Default::default() },
+      logging: false,
+    },
+  );
+  let shaken = tree_shake(
+    content.clone(),
+    TreeShakeOptions {
+      config: TreeShakeConfig::recommended(),
+      minify_options: None,
+      codegen_options: CodegenOptions::default(),
       logging: args.logging,
+    },
+  );
+  let shaken_minified = tree_shake(
+    shaken.codegen_return.code.clone(),
+    TreeShakeOptions {
+      config: TreeShakeConfig::disabled(),
+      minify_options: Some(MinifierOptions::default()),
+      codegen_options: CodegenOptions { minify: true, comments: false, ..Default::default() },
+      logging: false,
     },
   );
 
   let elapsed = start_time.elapsed();
 
-  for diagnostic in result.diagnostics.iter() {
+  for diagnostic in shaken.diagnostics.iter() {
     eprintln!("{}", diagnostic);
   }
 
-  eprintln!("[tree-shaker] Finished in {:?}", elapsed);
+  eprintln!("Completed in {:?}", elapsed);
+  eprintln!("Original: {}B", content.len());
+  eprintln!("Minified: {}B", minified.codegen_return.code.len());
+  eprintln!("  Shaken: {}B", shaken.codegen_return.code.len());
+  eprintln!("    Both: {}B", shaken_minified.codegen_return.code.len());
+  eprintln!(
+    "Minified/Both = {:.2}%",
+    (shaken_minified.codegen_return.code.len() as f64 / minified.codegen_return.code.len() as f64)
+      * 100.0
+  );
 
   // If the input file is dir/a.js, the output file will be dir/a.out.js
   let output_path = args.output.map_or_else(
@@ -86,7 +105,13 @@ fn main() {
     Ok(file) => file,
   };
 
-  match output_file.write_all(result.codegen_return.code.as_bytes()) {
+  let code = match (!args.no_shake, args.minify) {
+    (true, true) => shaken_minified.codegen_return.code,
+    (true, false) => shaken.codegen_return.code,
+    (false, true) => minified.codegen_return.code,
+    (false, false) => content,
+  };
+  match output_file.write_all(code.as_bytes()) {
     Err(why) => {
       eprintln!("Couldn't write to {}: {}", output_path.display(), why);
       std::process::exit(1);
