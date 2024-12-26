348030d672f26b7333e6821b924b79e9716343b5
_Kerman <kermanx@qq.com>
2024-12-26 22:12:29 +0800

    fix: crate exports

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,14 @@ mod utils;
 mod tests;
 
 use analyzer::Analyzer;
-pub use config::TreeShakeConfig;
-use oxc::allocator::Allocator;
-use oxc::parser::Parser;
-use oxc::span::SourceType;
-pub use tree_shaker::TreeShakeOptions;
-use tree_shaker::{TreeShakeReturn, TreeShaker};
-pub use utils::ast;
-pub use utils::dep_id::{self as dep};
+use oxc::{allocator::Allocator, parser::Parser, span::SourceType};
+use utils::{
+  ast,
+  dep_id::{self as dep},
+};
+
+pub use config::{TreeShakeConfig, TreeShakeJsxPreset};
+pub use tree_shaker::{TreeShakeOptions, TreeShakeReturn, TreeShaker};
 
 pub fn tree_shake(source_text: String, options: TreeShakeOptions) -> TreeShakeReturn {
   let source_type = SourceType::mjs().with_jsx(options.config.jsx.is_enabled());
