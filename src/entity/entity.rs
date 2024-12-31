26d37f5dcb351c154e97cf19ca9882873c82665d
_Kerman <kermanx@qq.com>
2024-12-31 15:50:28 +0800

    Revert "111", again

--- src/entity/entity.rs
+++ src/entity/entity.rs
@@ -94,8 +94,7 @@ pub trait EntityTrait<'a>: Debug {
     analyzer: &mut Analyzer<'a>,
     dep: Consumable<'a>,
     length: usize,
-    need_rest: bool,
-  ) -> (Vec<Entity<'a>>, Option<Entity<'a>>, Consumable<'a>) {
+  ) -> (Vec<Entity<'a>>, Entity<'a>, Consumable<'a>) {
     let (mut elements, rest, deps) = self.iterate(analyzer, dep);
     let extras = match elements.len().cmp(&length) {
       Ordering::Equal => Vec::new(),
@@ -109,19 +108,16 @@ pub trait EntityTrait<'a>: Debug {
       *element = analyzer.factory.computed(*element, deps);
     }
 
-    let rest_arr = need_rest.then(|| {
-      let rest_arr = analyzer.new_empty_array();
-      rest_arr.deps.borrow_mut().push(if extras.is_empty() && rest.is_none() {
-        analyzer.consumable((self, dep))
-      } else {
-        analyzer.consumable(dep)
-      });
-      rest_arr.elements.borrow_mut().extend(extras);
-      if let Some(rest) = rest {
-        rest_arr.init_rest(rest);
-      }
-      rest_arr as Entity<'a>
+    let rest_arr = analyzer.new_empty_array();
+    rest_arr.deps.borrow_mut().push(if extras.is_empty() && rest.is_none() {
+      analyzer.consumable((self, dep))
+    } else {
+      analyzer.consumable(dep)
     });
+    rest_arr.elements.borrow_mut().extend(extras);
+    if let Some(rest) = rest {
+      rest_arr.init_rest(rest);
+    }
 
     (elements, rest_arr, deps)
   }
