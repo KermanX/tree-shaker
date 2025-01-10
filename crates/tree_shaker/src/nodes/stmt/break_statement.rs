9b2a9adb6731e1af9ae03859a88407a3f2344b50
_Kerman <kermanx@qq.com>
2025-01-10 21:51:48 +0800

    chore: simplify break and continue statement

--- crates/tree_shaker/src/nodes/stmt/break_statement.rs
+++ crates/tree_shaker/src/nodes/stmt/break_statement.rs
@@ -1,28 +1,22 @@
 use crate::{analyzer::Analyzer, ast::AstKind2, transformer::Transformer};
 use oxc::ast::ast::{BreakStatement, Statement};
 
-#[derive(Debug, Default)]
-struct Data {
-  label_used: bool,
-}
-
 impl<'a> Analyzer<'a> {
   pub fn exec_break_statement(&mut self, node: &'a BreakStatement<'a>) {
     let label = node.label.as_ref().map(|label| label.name.as_str());
     if self.break_to_label(label) {
-      self.set_data(AstKind2::BreakStatement(node), Data { label_used: true });
+      self.consume(AstKind2::BreakStatement(node));
     }
   }
 }
 
 impl<'a> Transformer<'a> {
   pub fn transform_break_statement(&self, node: &'a BreakStatement<'a>) -> Option<Statement<'a>> {
-    let data = self.get_data::<Data>(AstKind2::BreakStatement(node));
-
     let BreakStatement { span, label } = node;
 
-    let label = data.label_used.then(|| self.clone_node(label)).flatten();
-
-    Some(self.ast_builder.statement_break(*span, label))
+    Some(self.ast_builder.statement_break(
+      *span,
+      if self.is_referred(AstKind2::BreakStatement(node)) { label.clone() } else { None },
+    ))
   }
 }
--- crates/tree_shaker/src/nodes/stmt/continue_statement.rs
+++ crates/tree_shaker/src/nodes/stmt/continue_statement.rs
@@ -1,16 +1,11 @@
 use crate::{analyzer::Analyzer, ast::AstKind2, transformer::Transformer};
 use oxc::ast::ast::{ContinueStatement, Statement};
 
-#[derive(Debug, Default)]
-struct Data {
-  label_used: bool,
-}
-
 impl<'a> Analyzer<'a> {
   pub fn exec_continue_statement(&mut self, node: &'a ContinueStatement<'a>) {
     let label = node.label.as_ref().map(|label| label.name.as_str());
     if self.continue_to_label(label) {
-      self.set_data(AstKind2::ContinueStatement(node), Data { label_used: true });
+      self.consume(AstKind2::ContinueStatement(node));
     }
   }
 }
@@ -20,14 +15,11 @@ impl<'a> Transformer<'a> {
     &self,
     node: &'a ContinueStatement<'a>,
   ) -> Option<Statement<'a>> {
-    let data = self.get_data::<Data>(AstKind2::ContinueStatement(node));
+    let ContinueStatement { span, label } = node;
 
-    let ContinueStatement { span, .. } = node;
-
-    Some(if data.label_used {
-      Statement::ContinueStatement(self.ast_builder.alloc(self.clone_node(node)))
-    } else {
-      self.ast_builder.statement_continue(*span, None)
-    })
+    Some(self.ast_builder.statement_continue(
+      *span,
+      if self.is_referred(AstKind2::ContinueStatement(node)) { label.clone() } else { None },
+    ))
   }
 }
